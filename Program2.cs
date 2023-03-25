db42915868b000a02edde473bfa42f88ed061085
Avgust Zagorodnikov <zagavg@yandex.ru>
2023-03-25 16:38:04 +0300

    Update

--- Program2.cs
+++ Program2.cs
@@ -4,16 +4,66 @@ using static System.Math;
 static partial class Program
 {
     static bool _stop;
+    private static long _moveTime;
+    //     System.NullReferenceException: Object reference not set to an instance of an object.
+    //    at Neuron.WriteValue() in C:\Users\Лисёнок\Documents\Talk\NeuralNetwork.cs:line 99
+    //    at NeuralNetwork.Use(Single[] data) in C:\Users\Лисёнок\Documents\Talk\NeuralNetwork.cs:line 19
+    //    at Program.GetResolution(NeuralNetwork network) in C:\Users\Лисёнок\Documents\Talk\Simulation.cs:line 237
+    //    at Program.Main() in C:\Users\Лисёнок\Documents\Talk\Program2.cs:line 51
     static void Main()
     {
         try
         {
-            var neuralNetwork = new NeuralNetwork("5,4", ActivateFunction.Lin);
-            EvolutionProcess process = new EvolutionProcess(Evolution.GetNeuralNetwork(neuralNetwork, ActivateFunction.Lin, -5, 5), IsGoodSystem, 0.3, new List<NeuralNetwork>(20));
+            Error.Write();
+            var prob = 0;
+            for (int column = 0; column < _field.GetLength(0); column++)
+            {
+                for (int row = 0; row < _field.GetLength(1); row++)
+                {
+                    Error.Write();
+                    if (column == 0 || column == _field.GetLength(0) - 1 || row == 0 || row == _field.GetLength(1) - 1)
+                    {
+                        _field[column, row] = Cell.Barrier;
+                    }
+                    else
+                    {
+                        _field[column, row] = _rng.NextDouble() > prob ? Cell.Empty : Cell.Projectile;
+                    }
+                    Error.Write();
+                }
+            }
+            Error.Write();
+            _field[20, 20] = Cell.Exit;
+            _playerX = 1;
+            _playerY = 1;
+            var neuralNetwork = new NeuralNetwork("4,4", ActivateFunction.Lin);
+            var array = new NeuralNetwork[20];
+            Array.Fill(array, Evolution.GetNeuralNetwork(neuralNetwork, ActivateFunction.Lin, -5, 5));
+            EvolutionProcess process = new EvolutionProcess(Evolution.GetNeuralNetwork(neuralNetwork, ActivateFunction.Lin, -5, 5), IsGoodSystem, 0.3F, new List<NeuralNetwork>(array));
+            Console.CursorVisible = false;
+            var i = 0;
+            Error.Write();
             while (!_stop)
             {
+                i++;
+                process.Selection(100);
                 DrawField();
+                if (i == 40)
+                {
+                    _stop = true;
+                }
+                _field[1, 1] = Cell.Player;
             }
+            Error.Write();
+            for (int j = 0; j < 200; j++)
+            {
+                DrawField();
+                TimeSync();
+                GetResolution(process.Networks[0]);
+                TimeSync();
+                _moveTime = _time;
+            }
+            Error.Write();
         }
         catch (Exception exception)
         {
@@ -22,6 +72,10 @@ static partial class Program
         finally
         {
             Console.ReadKey();
+        }
+        static void TimeSync()
+        {
+            _time = (long)(Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency * 1000);
         }
     }
 }
